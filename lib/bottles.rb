65c77bfeb515f4839c74456369af9e3837552067
Matt Mills <sunrisetimes@gmail.com>
2013-12-09 23:11:33 -0800

    even better with verse_n extracted

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -19,8 +19,12 @@ class Verse
     @n = n
   end
   
+  def verse_n
+    "#{n} bottles of beer on the wall, #{n} bottles of beer.\nTake one down and pass it around, #{n-1} bottles of beer on the wall.\n"
+  end
+  
   def verses
-   Hash.new("#{n} bottles of beer on the wall, #{n} bottles of beer.\nTake one down and pass it around, #{n-1} bottles of beer on the wall.\n").merge!({
+   Hash.new(verse_n).merge!({
                0 => "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n",
                1 => "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n",
                2 => "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n",
